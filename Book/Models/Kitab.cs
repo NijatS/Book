7e99e7f5c16a089ab530b56e5227c3fb8f18f73d
NijatS <nijatjs@code.edu.az>
2023-03-08 15:30:40 +0400

    done

---
+++ Book/Models/Kitab.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.Models
+{
+    public class Kitab
+    {
+        private int _pageCount;
+        private string _name;
+        public string Name { get=> _name; set
+            {
+                if(value.Length >= 3)
+                {
+                    _name = value;
+                }
+
+            } }
+        public int PageCount { get=>_pageCount; set
+            {
+                if (value >= 10)
+                {
+                    _pageCount = value;
+                }
+            }
+
+        }
+        public Kitab(string name, int pageCount)
+        {
+
+                Name = name;
+                PageCount = pageCount;
+            
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name : " + Name + " Count : " + PageCount);
+        }
+
+    }
+}
---
+++ Book/Program.cs
@@ -0,0 +1,72 @@
+﻿
+
+using Book.Models;
+
+
+Console.Write("Please enter size of array :");
+int size = int.Parse(Console.ReadLine());
+
+Kitab[] books = new Kitab[size];
+AddBook(ref books);
+Show(books);
+
+Kitab[] AddBook(ref Kitab[] books)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Enter Name : ");
+        string name = Console.ReadLine();
+        Console.Write("Enter Count : ");
+        int count = int.Parse(Console.ReadLine());
+        while (!CheckNameandPage(name,count))
+        {
+            Console.WriteLine("Again!!!");
+            Console.Write("Enter Name : ");
+            name = Console.ReadLine();
+            Console.Write("Enter Count : ");
+            count = int.Parse(Console.ReadLine());
+        }
+        while (!CheckSameName(name))
+        {
+            Console.WriteLine("Same Name!!!");
+            Console.Write("Enter Name : ");
+            name = Console.ReadLine();
+            Console.Write("Enter Count : ");
+            count = int.Parse(Console.ReadLine());
+        }
+        books[i] = new Kitab(name, count);
+    }
+    return books;
+}
+
+void Show(Kitab[] books)
+{
+    foreach (var book in books)
+    {
+        book.ShowInfo();
+    }
+}
+bool CheckNameandPage(string name,int count)
+{
+    if(string.IsNullOrEmpty(name) && name.Length <3 ) return false;
+    if(count < 10) return false;
+
+    return true;
+
+}
+
+bool CheckSameName(string name)
+{
+
+    foreach (Kitab book in books)
+    {
+        if (book != null)
+        {
+            string nameBook = book.Name;
+            if (name.Equals(nameBook)) return false;
+        }
+    }
+    return true;
+    
+}
+
